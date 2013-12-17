4ccb7d3515d7101a3d9da5b413b2618f45b45d3e
Dan Whisman <whisman1@illinois.edu>
2013-12-17 08:08:04 -0600

    Copied bcsd to relay project, started acs.

---
+++ projects/acs/acs.h
@@ -0,0 +1,19 @@
+/*
+ * adc.h
+ * 
+ * Attempt to use the A/D converter on the expansion port and report
+ * the value to the shell.
+ */
+
+#ifndef _ADC_H_
+#define _ADC_H_
+
+#include "contiki.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif /* _ADC_H_ */
+
+
